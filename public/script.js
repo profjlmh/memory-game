a0cdf885f88b12d519176d1ce875fa638267b127
Chefo <jose.martinez09@iest.edu.mx>
2025-03-19 22:18:47 -0600

    Subiendo Memory Game con WebSockets

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const socket = new WebSocket('ws://localhost:8080');
+const socket = new WebSocket('ws:memory-game-production-ce2b.up.railway.app');
 let playerId;
 let score = 0;
 let gameBoard = document.getElementById('gameBoard');
